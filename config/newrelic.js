b5a9af09e9661b4fbee4e6e5cd51f565abf76dd4
Leo <zmliu0077@gmail.com>
2016-03-08 16:19:45 +0800

    add new relic

--- config/newrelic.js
+++ config/newrelic.js
@@ -4,7 +4,7 @@
  * See lib/config.defaults.js in the agent distribution for a more complete
  * description of configuration variables and their potential values.
  */
-modules.exports.newrelic = {
+module.exports.newrelic = {
 
   /**
    * Array of application names.
